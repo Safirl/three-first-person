3eaff5fab4279d0e3ff0438ec37cea2eae9adf06
neroliprak <neroliprak@gmail.com>
2026-04-08 10:58:08 +0200

    add octreeCollision

--- src/templates/world/NatureWorld.ts
+++ src/templates/world/NatureWorld.ts
@@ -1,14 +1,28 @@
 import { Environment, Experience, Floor, World } from "base-experience";
+// import type FirstPersonCamera from "../../camera/FirstPersonCamera";
+import { Wall } from "../objects/Wall";
+import type FirstPersonCamera from "../../camera/FirstPersonCameraOctree";
 
 export default class NatureWorld extends World {
   declare experience: Experience;
   declare environment: Environment;
   declare resources: Experience["resources"];
   declare floor: Floor;
+  declare wall: Wall;
 
   onResourcesLoaded() {
     this.floor = new Floor();
     this.floor.mesh.position.y = -5;
+    this.wall = new Wall();
+    this.wall.mesh.position.y = 0;
+    this.wall.mesh.position.x = -5;
     this.environment = new Environment();
+
+    // Ajouter les collisions par rapport à la capsule du player
+    const camera = Experience.instance?.camera as FirstPersonCamera;
+    if (camera.worldOctree) {
+      camera.worldOctree.fromGraphNode(this.floor.mesh);
+      camera.worldOctree.fromGraphNode(this.wall.mesh);
+    }
   }
 }
