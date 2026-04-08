af86b705c57f6c88f8bcfa8211a88544ac1c6c28
neroliprak <neroliprak@gmail.com>
2026-04-08 13:45:27 +0200

    last commit

--- src/templates/world/CollisionTemplateWorld.ts
+++ src/templates/world/CollisionTemplateWorld.ts
@@ -2,6 +2,7 @@ import { Environment, Experience, Floor, World } from "base-experience";
 import type FirstPersonCameraOctree from "../../camera/FirstPersonCameraOctree";
 import Wall from "../objects/Wall";
 
+
 export default class CollisionTemplateWorld extends World {
   declare experience: Experience;
   declare environment: Environment;
