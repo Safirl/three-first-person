4a555f366cc7bb0fc95b1780493954899214c8f3
neroliprak <neroliprak@gmail.com>
2026-04-08 11:06:37 +0200

    add wall geometry test

--- src/templates/world/NatureWorld.ts
+++ src/templates/world/NatureWorld.ts
@@ -1,20 +1,25 @@
 import { Environment, Experience, Floor, World } from "base-experience";
 import type FirstPersonCameraOctree from "../../camera/FirstPersonCameraOctree";
+import Wall from "../objects/Wall";
 
 export default class NatureWorld extends World {
   declare experience: Experience;
   declare environment: Environment;
   declare resources: Experience["resources"];
   declare floor: Floor;
+  declare wall: Wall;
 
   onResourcesLoaded() {
     this.floor = new Floor();
     this.floor.mesh.position.y = -5;
+    this.wall = new Wall();
+    this.wall.mesh.position.y = -5;
     this.environment = new Environment();
 
     const camera = Experience.instance?.camera as FirstPersonCameraOctree;
     if (camera.worldOctree) {
       camera.worldOctree.fromGraphNode(this.floor.mesh);
+      camera.worldOctree.fromGraphNode(this.wall.mesh);
     }
   }
 }
